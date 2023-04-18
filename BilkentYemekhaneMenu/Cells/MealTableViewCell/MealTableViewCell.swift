ef1f4a7d0c1523b4eef0374121dfb5b07f3b6d49
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-04-18 12:06:11 +0300

    Courses Table View Cell is added.

---
+++ BilkentYemekhaneMenu/Cells/MealTableViewCell/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  BilkentYemekhaneMenu
+//
+//  Created by Asım Altınışık on 17.04.2023.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
