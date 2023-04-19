9a33310a747d9385de693980c97fa9b3cb078d2b
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-04-19 22:44:05 +0300

    Nutrition Facts Table View Cell is added.

---
+++ BilkentYemekhaneMenu/Cells/NutritionFactsTableVİewCell/NutritionFactsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NutritionFactsTableViewCell.swift
+//  BilkentYemekhaneMenu
+//
+//  Created by Asım Altınışık on 19.04.2023.
+//
+
+import UIKit
+
+class NutritionFactsTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var energyLabel: UILabel!
+    @IBOutlet weak var carbonhydrateLabel: UILabel!
+    @IBOutlet weak var proteinLabel: UILabel!
+    @IBOutlet weak var fatLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
