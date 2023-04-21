0e0cf0f181dfbcb0daaa3eb37dcf24675e45ceb6
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-04-21 09:31:48 +0300

    MealTableViewCell is added.

--- BilkentYemekhaneMenu/Cells/MealTableViewCell/MealTableViewCell.swift
+++ BilkentYemekhaneMenu/Cells/MealTableViewCell/MealTableViewCell.swift
@@ -7,18 +7,29 @@
 
 import UIKit
 
+protocol MealTableViewCellDelegate: AnyObject {
+    func likeButtonTapped(for course: Course, rowNumber: [IndexPath])
+}
+
 class MealTableViewCell: UITableViewCell {
     @IBOutlet weak var courseName: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
+    var course: Course?
+    var rowNumber: [IndexPath]?
+    weak var delegate: MealTableViewCellDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBAction func likeButtonTapped(_ sender: Any) {
+        guard let course = course else { return }
+        delegate?.likeButtonTapped(for: course, rowNumber: rowNumber ?? [])
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
 }
