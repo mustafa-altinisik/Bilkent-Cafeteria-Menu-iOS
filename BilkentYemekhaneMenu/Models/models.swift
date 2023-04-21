9b97b1e88de8a7a0f90565bc7ef4091b0dbe1143
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-04-21 09:33:52 +0300

    SingleNotification structure is added. Meal strut is modified. nutritionFacts is now a [String: String]

--- BilkentYemekhaneMenu/Models/models.swift
+++ BilkentYemekhaneMenu/Models/models.swift
@@ -13,7 +13,7 @@ struct Meal: Codable {
     let englishName: String
     let id: Int
     let name: String
-    let nutritionFacts: String
+    let nutritionFacts: [String: String] // modified to be a dictionary
 
     private enum CodingKeys: String, CodingKey {
         case courses
@@ -22,6 +22,21 @@ struct Meal: Codable {
         case name
         case nutritionFacts = "nutrition_facts"
     }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        courses = try values.decode([Course].self, forKey: .courses)
+        englishName = try values.decode(String.self, forKey: .englishName)
+        id = try values.decode(Int.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        
+        if let nutritionFactsDict = try? values.decode([String: String].self, forKey: .nutritionFacts) {
+            nutritionFacts = nutritionFactsDict
+        } else {
+            nutritionFacts = [:] // default empty dictionary if key not found
+        }
+    }
+
 }
 
 struct Course: Codable {
@@ -33,5 +48,21 @@ struct Course: Codable {
         case englishName = "english_name"
         case id
         case name
+    }
+}
+
+struct SingleNotification: Equatable {
+    let name: String
+    let hour: Int
+    let minute: Int
+    let days: [Int]
+    let isOn: Bool
+    
+    static func == (lhs: SingleNotification, rhs: SingleNotification) -> Bool {
+        return lhs.name == rhs.name &&
+            lhs.hour == rhs.hour &&
+            lhs.minute == rhs.minute &&
+            lhs.days == rhs.days &&
+            lhs.isOn == rhs.isOn
     }
 }
